72de3e289bec521a8de4a485b11ab7dbcfbd51ab
Keagan Herman <Hermanleah75@gmail.com>
2022-08-04 09:57:30 +0200

    Hope it pushes

---
+++ app/src/main/java/com/varsitycollege/weatherapp/NetworkUtil.java
@@ -0,0 +1,69 @@
+package com.varsitycollege.weatherapp;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtil
+{
+    private static final String BASE_URL = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/305605";
+    private static final String API_KEY = "\tQLu3p2SKVfqwnfU9jRHTPRpuWOwiEuGI";
+    private static final String PARAM_API_KEY = "apikey";
+    private static final String METRIC_PARAM = "metric";
+    private static final String METRIC_VALUE = "true";
+    private static final String TAG = "NETWORK_UTIL";
+
+    public static URL buildURL()
+    {
+        Uri uri = Uri.parse(BASE_URL)
+                .buildUpon()
+                .appendQueryParameter(PARAM_API_KEY, API_KEY)
+                .appendQueryParameter(METRIC_PARAM, METRIC_VALUE)
+                .build();
+
+        URL url = null;
+
+        try {
+            url = new URL((uri).toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+    public static String getResponse(URL url) throws IOException {
+
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+        try
+        {
+            //gets input from the http GET request response
+            InputStream in = httpURLConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            //find next line
+            scanner.useDelimiter("//A");//delimiter for JSON
+            boolean hasInput = scanner.hasNext();
+            if (hasInput)
+            {
+                return scanner.next();
+            }
+            else
+            {
+                Log.i(TAG,"getResponse: " + scanner.next());
+                return null;
+            }
+
+        }
+        finally
+        {
+            httpURLConnection.disconnect();
+        }
+    }
+}
